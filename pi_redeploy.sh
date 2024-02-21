19065532a25e0a037dd34b74d7fd008581b1b5e0
hongru <renhongru1990@qq.com>
2024-02-21 19:49:52 +0800

    last commit

--- pi_redeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-MODULE=wot_api
-API=api.yaml
-TARGET=flask_out
-
-( set -x ; python3 m2m.py )
-( set -x ; openapi-generator-cli generate -i ${API} -g python-flask -o ${TARGET} -c config.json )
-( set -x ; python3 post_generation.py ${TARGET} ${MODULE} ${API} )
-( set -x ; cp -r __main__.py implementations/ ${TARGET}/${MODULE} )
